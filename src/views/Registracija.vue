eb46671de2d1ceab009159d4ea0202e4654ebc46
stopcagic <49293128+Saud-T@users.noreply.github.com>
2020-04-14 16:19:35 +0200

    bnt

--- src/views/Registracija.vue
+++ src/views/Registracija.vue
@@ -1,46 +1,64 @@
 <template>
- <div id="signup" class="container">
- <div class="card">
- <div class="card-body">
-   <h1>Regitracija</h1>
-<div class="input-group mb-5">
-  <div class="input-group-prepend">
-    <span class="input-group-text"><i class="far fa-envelope"></i></span>
-  </div>
-  <input type="email" name="email" required class="form-control" placeholder="Email" >
-</div>
-<div class="input-group mb-5">
-  <div class="input-group-prepend">
-    <span class="input-group-text"><i class="far fa-envelope"></i></span>
-  </div>
-  <input type="email" name="email" required class="form-control" placeholder="Ponovite Email" >
-</div>
+  <div id="signup" class="container">
+    <div class="card">
+      <div class="card-body">
+        <h1>Regitracija</h1>
+        <div class="input-group mb-5">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="far fa-envelope"></i>
+            </span>
+          </div>
+          <input type="email" name="email" required class="form-control" placeholder="Email" />
+        </div>
+        <div class="input-group mb-5">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="far fa-envelope"></i>
+            </span>
+          </div>
+          <input
+            type="email"
+            name="email"
+            required
+            class="form-control"
+            placeholder="Ponovite Email"
+          />
+        </div>
 
-<div class="input-group mb-5">
-  <div class="input-group-prepend">
-    <span class="input-group-text"><i class="fas fa-key"></i></span>
-  </div>
-  <input type="password" name="lozinka" required class="form-control" placeholder="Lozinka" >
-</div>
-<div class="input-group mb-5">
-  <div class="input-group-prepend">
-    <span class="input-group-text"><i class="fas fa-key"></i></span>
-  </div>
-  <input type="password" name="lozinka" required class="form-control" placeholder="Ponovite Lozinku" >
-</div>
+        <div class="input-group mb-5">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="fas fa-key"></i>
+            </span>
+          </div>
+          <input type="password" name="lozinka" required class="form-control" placeholder="Lozinka" />
+        </div>
+        <div class="input-group mb-5">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="fas fa-key"></i>
+            </span>
+          </div>
+          <input
+            type="password"
+            name="lozinka"
+            required
+            class="form-control"
+            placeholder="Ponovite Lozinku"
+          />
+        </div>
 
-<div>
-    <button type="submit" class="btn btn-primary btn-lg">Reigstriraj se</button>
-</div>
+        <div>
+          <button type="submit" class="btn btn-primary btn-lg">Reigstriraj se</button>
+        </div>
 
-<p class="text-center">
-   Imate račun?
-   <router-link to="/prijava" href="button">Prijavite se</router-link>
-</p>
-
-
- </div>
+        <p class="text-center">
+          Imate račun?
+          <router-link to="/prijava" href="button">Prijavite se</router-link>
+        </p>
       </div>
+    </div>
   </div>
 </template>
 
@@ -49,29 +67,28 @@
   margin: 0px auto 10x;
   max-width: 800px;
   padding: 0px;
-  font-family: 'Gotu', sans-serif;
+  font-family: "Gotu", sans-serif;
   text-align: center;
-    
 }
-.card{
+.card {
   margin: 0px auto 10px;
   width: 85%;
   background-color: #708090;
   border: 1px solid white;
   color: #272727;
   padding-top: 5%;
-  color: #FFF5EE;
+  color: #fff5ee;
 }
-.input-group{
-    width: 50%;
-    margin: 0 auto;
+.input-group {
+  width: 50%;
+  margin: 0 auto;
 }
-input[type=email],[type=password]{
+input[type="email"],
+[type="password"] {
   background: transparent;
-  
 }
-::placeholder{
-  color: #FFF5EE;
+::placeholder {
+  color: #fff5ee;
 }
 .btn {
   margin-bottom: 5%;
@@ -79,18 +96,16 @@ input[type=email],[type=password]{
   height: 60px;
   cursor: pointer;
   background: transparent;
-  border: 1px solid #4169E1;
+  border: 0.3px solid #4169e1;
   outline: none;
-  transition: 1s ease-in-out;
-  color: #FFF5EE;
+  transition: 0.3s ease-in-out;
+  color: #fff5ee;
 }
 .btn:hover {
   transition: 1s ease-in-out;
-  background: #4169E1;
+  background: #4169e1;
 }
-h1{
+h1 {
   margin-bottom: 5%;
 }
-
-
 </style>
