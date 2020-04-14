aa3cab3de95e3d3d91be6807210bd0805d6b0271
stopcagic <49293128+Saud-T@users.noreply.github.com>
2020-04-14 16:16:50 +0200

    ok

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,78 +1,79 @@
 <template>
   <div id="home" class="container">
     <template>
-<div id="login" class="container">
- <div class="card">
- <div class="card-body">
-  <h1>Prijava</h1>
-  
- 
-  <div class="input-group mb-5">
-  <div class="input-group-prepend">
-    <span class="input-group-text"><i class="far fa-envelope"></i></span>
-  </div>
-  <input type="email" name="email" required class="form-control" placeholder="Email" >
-</div>
-  <div class="input-group mb-5">
-  <div class="input-group-prepend">
-    <span class="input-group-text"><i class="fas fa-key"></i></span>
-  </div>
-  <input type="password" name="lozinka" required class="form-control" placeholder="Lozinka" >
-</div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Zapamti me</label>
-  </div>
-  
-<div>
-    <button type="submit" class="btn btn-primary btn-lg">Prijavi se</button>
-</div>
+      <div id="login" class="container">
+        <div class="card">
+          <div class="card-body">
+            <h1>Prijava</h1>
 
-<p class="text-center">
-   Nemate račun?
-   <router-link to="/registracija" href="button">Registriraj se</router-link>
-</p>
+            <div class="input-group mb-5">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="far fa-envelope"></i>
+                </span>
+              </div>
+              <input type="email" name="email" required class="form-control" placeholder="Email" />
+            </div>
+            <div class="input-group mb-5">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="fas fa-key"></i>
+                </span>
+              </div>
+              <input
+                type="password"
+                name="lozinka"
+                required
+                class="form-control"
+                placeholder="Lozinka"
+              />
+            </div>
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+              <label class="form-check-label" for="exampleCheck1">Zapamti me</label>
+            </div>
 
+            <div>
+              <button type="submit" class="btn btn-primary btn-lg">Prijavi se</button>
+            </div>
 
- </div>
+            <p class="text-center">
+              Nemate račun?
+              <router-link to="/registracija" href="button">Registriraj se</router-link>
+            </p>
+          </div>
+        </div>
       </div>
+    </template>
   </div>
 </template>
  
-  </div>
-</template>
 
 <script>
-
-
 export default {
-  name: 'Home',
-  
-}
+  name: "Home"
+};
 </script>
 <style scoped>
-
-.card{
+.card {
   margin: 0px auto 10px;
   width: 85%;
   background-color: #708090;
   border: 1px solid white;
   color: #272727;
   padding-top: 5%;
-    color: #FFF5EE;
+  color: #fff5ee;
 }
 
 .container {
   margin: 0px auto 10x;
   max-width: 800 px;
   padding: 0px;
-  font-family: 'Gotu', sans-serif;
+  font-family: "Gotu", sans-serif;
   text-align: center;
-  
-  
 }
-a{
-  color:#4169E1
+a {
+  color: #4169e1;
 }
 
 .btn {
@@ -81,36 +82,31 @@ a{
   height: 60px;
   cursor: pointer;
   background: transparent;
-  border: 1px solid #4169E1;
+  border: 1px solid #4169e1;
   outline: none;
-  transition: 1s ease-in-out;
-  color: #FFF5EE;
+  transition: 0.3s ease-in-out;
+  color: #fff5ee;
   margin-top: 5%;
 }
 .btn:hover {
-  transition: 1s ease-in-out;
-  background: #4169E1;
+  transition: 0.3s ease-in-out;
+  background: #4169e1;
 }
 
-input[type=email],[type=password]{
+input[type="email"],
+[type="password"] {
   background: transparent;
-  
 }
 
-::placeholder{
-  color: #FFF5EE;
+::placeholder {
+  color: #fff5ee;
 }
-h1{
+h1 {
   margin-bottom: 5%;
 }
-.input-group{
-   
-    width: 50%;
-    margin: 0 auto;
+.input-group {
+  width: 50%;
+  margin: 0 auto;
 }
-
-
-
-
 </style>
 
