dbf925313b9c53e24cb539e56c7a392269e5b34e
stopcagic <49293128+Saud-T@users.noreply.github.com>
2020-05-09 18:59:07 +0200

    fixed bug

--- BACKEND/src/routes/protectedRoutes.js
+++ BACKEND/src/routes/protectedRoutes.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 export default (req, res, next) => {
     const token = req.header('auth-token')
-    if (!token) res.status(401).send('Access deined.')
+    if (!token) return res.status(401).send('Access deined.')
 
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET)
