7a5418930590f892393d0bd8754abbeca6ae8fc2
stopcagic <49293128+Saud-T@users.noreply.github.com>
2020-06-14 00:03:12 +0200

    Update predavaci.js

--- BACKEND/src/routes/predavaci.js
+++ BACKEND/src/routes/predavaci.js
@@ -7,7 +7,7 @@ const router = express.Router()
 
 router.get('/predavaci', verify, async (req, res) => {
     let db = await connect()
-
+    //samo dodati find(where faks = fipu or what not)
     let cursor = await db.collection("predavaci").find()
     let results = await cursor.toArray()
 
