e163f58312f4388cd7587d9769aa0c2ceb4ab00c
MarkoSolic <58081989+MarkoSolic@users.noreply.github.com>
2020-06-30 19:09:13 +0200

    12

--- FRONTEND/src/views/Registracija.vue
+++ FRONTEND/src/views/Registracija.vue
@@ -1,103 +1,101 @@
 <template>
-  <modal height="auto" name="registracija-modal">
-    <div id="signup" class="container-fluid">
-      <div class="card card-registracija">
-        <h1>
-          Registracija
-          <hr />
-        </h1>
-        <form @submit.prevent="signup">
-          <div class="input-group mb-5">
-            <div class="input-group-prepend">
-              <span class="input-group-text">
-                <i class="far fa-envelope"></i>
-              </span>
-            </div>
-            <input
-              type="email"
-              name="email"
-              required
-              class="form-control"
-              placeholder="Email"
-              v-model="email"
-            />
+  <modal resizable:true height="auto" name="registracija-modal">
+    <div id="signup" class="container-fluid card-registracija">
+      <h1>
+        Registracija
+        <hr />
+      </h1>
+      <form @submit.prevent="signup">
+        <div class="input-group mb-5">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="far fa-envelope"></i>
+            </span>
           </div>
-          <div class="input-group mb-5">
-            <div class="input-group-prepend">
-              <span class="input-group-text">
-                <i class="far fa-envelope"></i>
-              </span>
-            </div>
-            <input
-              type="email"
-              name="email"
-              required
-              class="form-control"
-              placeholder="Ponovite Email"
-              v-model="newEmail"
-            />
+          <input
+            type="email"
+            name="email"
+            required
+            class="form-control"
+            placeholder="Email"
+            v-model="email"
+          />
+        </div>
+        <div class="input-group mb-5">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="far fa-envelope"></i>
+            </span>
           </div>
+          <input
+            type="email"
+            name="email"
+            required
+            class="form-control"
+            placeholder="Ponovite Email"
+            v-model="newEmail"
+          />
+        </div>
 
-          <div class="input-group mb-5">
-            <div class="input-group-prepend">
-              <span class="input-group-text">
-                <i class="fas fa-key"></i>
-              </span>
-            </div>
-            <input
-              type="password"
-              name="password"
-              required
-              class="form-control"
-              placeholder="Lozinka"
-              v-model="password"
-            />
+        <div class="input-group mb-5">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="fas fa-key"></i>
+            </span>
           </div>
-          <div class="input-group mb-5">
-            <div class="input-group-prepend">
-              <span class="input-group-text">
-                <i class="fas fa-key"></i>
-              </span>
-            </div>
-            <input
-              type="password"
-              name="password"
-              required
-              class="form-control"
-              placeholder="Ponovite Lozinku"
-              v-model="newPassword"
-            />
+          <input
+            type="password"
+            name="password"
+            required
+            class="form-control"
+            placeholder="Lozinka"
+            v-model="password"
+          />
+        </div>
+        <div class="input-group mb-5">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="fas fa-key"></i>
+            </span>
           </div>
-          <div class="input-group mb-5">
-            <div class="input-group-prepend">
-              <span class="input-group-text">
-                <i class="fas fa-university"></i>
-              </span>
-            </div>
-            <label type="faks" for="faks" required></label>
-            <select class="form-control" id="faks" v-model="faks">
-              <option selected>Odaberite...</option>
-              <option value="fet">Fakultet ekonimije i turizma "Dr. Mijo Mirković"</option>
-              <option value="fooz">Fakultet za odgojne i obrazovne znanosti</option>
-              <option value="ffpu">Filozofski fakultet</option>
-              <option value="fitiks">
-                Fakultet za interdisciplinarne, talijanske i kulturološke
-                studije
-              </option>
-              <option value="fipu">Fakultet informatike u Puli</option>
-              <option value="mfpu">Medicinski fakultet u Puli</option>
-            </select>
+          <input
+            type="password"
+            name="password"
+            required
+            class="form-control"
+            placeholder="Ponovite Lozinku"
+            v-model="newPassword"
+          />
+        </div>
+        <div class="input-group mb-5">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="fas fa-university"></i>
+            </span>
           </div>
+          <label type="faks" for="faks" required></label>
+          <select class="form-control" id="faks" v-model="faks">
+            <option selected>Odaberite...</option>
+            <option value="fet">Fakultet ekonimije i turizma "Dr. Mijo Mirković"</option>
+            <option value="fooz">Fakultet za odgojne i obrazovne znanosti</option>
+            <option value="ffpu">Filozofski fakultet</option>
+            <option value="fitiks">
+              Fakultet za interdisciplinarne, talijanske i kulturološke
+              studije
+            </option>
+            <option value="fipu">Fakultet informatike u Puli</option>
+            <option value="mfpu">Medicinski fakultet u Puli</option>
+          </select>
+        </div>
 
-          <div>
-            <button type="submit" class="btn btn-primary btn-lg">Registriraj se</button>
-          </div>
-        </form>
-        <p class="text-center">
-          Imate račun?
-          <router-link to="/" href="button">Prijavite se</router-link>
-        </p>
-      </div>
+        <div>
+          <button type="submit" class="btn btn-primary btn-lg">Registriraj se</button>
+        </div>
+      </form>
+      <p class="text-center">
+        Imate račun?
+        <router-link to="/" href="button">Prijavite se</router-link>
+      </p>
     </div>
   </modal>
 </template>
@@ -135,12 +133,13 @@ export default {
 </script>
 <style scoped>
 .card-registracija .input-group {
-  width: 50%;
+  width: 70%;
 }
 .card-registracija {
   color: white;
   border: none;
-  border-radius: 15px;
+  padding-top: 10%;
+  padding-bottom: 10%;
   background-image: url("../assets/card-pozadina.svg");
 }
 .card-registracija .btn {
@@ -158,5 +157,11 @@ export default {
   transition: 0.3s ease-in-out;
   background: #00b7ff;
   color: white;
+}
+
+@media (width: 375px) {
+  .modal-view {
+    width: 20%;
+  }
 }
 </style>
