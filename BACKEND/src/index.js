d85cca733babb49b3a81c2935dcbf0688acbc6b7
stopcagic <49293128+stopcagic@users.noreply.github.com>
2020-07-26 14:59:49 +0200

    fixani warningsi od SameSite

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -1,6 +1,7 @@
 import express, { urlencoded } from 'express';
 import cors from 'cors'
 import dotenv from 'dotenv'
+import CookieParser from 'cookie-parser'
 
 import auth from './routes/auth'
 import predavaci from './routes/predavaci'
@@ -14,7 +15,7 @@ dotenv.config()
 app.use(cors())
 app.use(urlencoded({ extended: true }))
 app.use(express.json())
-
+app.use(CookieParser())
 
 app.use('/predavaci', predavaci)
 app.use('/user', auth)
