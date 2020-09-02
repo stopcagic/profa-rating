ed0d3901bea29e7c0c6ae5b96f1b2167e17eda95
stopcagic <49293128+stopcagic@users.noreply.github.com>
2020-09-02 22:12:01 +0200

    Update header.vue

--- FRONTEND/src/components/header.vue
+++ FRONTEND/src/components/header.vue
@@ -56,7 +56,7 @@ export default {
       this.$router.go();
     },
     openLogin() {
-      this.$store.commit("prijava");
+      this.$store.commit('prijava');
       console.log(this.$store.state.prijavi_se);
     },
     myFunction() {
