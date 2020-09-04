45724670aadff440748a25f4a9f21537706a1a21
MarkoSolic <58081989+MarkoSolic@users.noreply.github.com>
2020-09-05 00:27:11 +0200

    1

--- FRONTEND/src/services/index.js
+++ FRONTEND/src/services/index.js
@@ -1,142 +1,144 @@
-import axios from 'axios'
-import router from '@/router'
+import axios from "axios";
+import router from "@/router";
 
 let Services = axios.create({
-    baseURL: 'http://localhost:3000',
-    timeout: 10000
-})
-
-Services.interceptors.request.use((request) => {
-    try {
-        request.headers['Authorization'] = 'Bearer ' + auth.getToken();
-    } catch (e) {
-        console.error(e);
-    }
-    return request;
+  baseURL: "https://aqueous-dawn-46215.herokuapp.com/",
+  timeout: 10000,
 });
 
-Services.interceptors.response.use((response) => response, (error) => {
+Services.interceptors.request.use((request) => {
+  try {
+    request.headers["Authorization"] = "Bearer " + auth.getToken();
+  } catch (e) {
+    console.error(e);
+  }
+  return request;
+});
+
+Services.interceptors.response.use(
+  (response) => response,
+  (error) => {
     if (error.response.status < 300 || error.response.status > 599) {
-        auth.logout()
-        router.go()
+      auth.logout();
+      router.go();
     }
-    return error.response
-})
+    return error.response;
+  }
+);
 
 const profs = {
-    async getNeoznacene(faks) {
-        const user = auth.getUser()
-        
-        let response = await Services.post(`predavaci/${faks}`,{
-            email: user.email
-        })
-        let data = await response.data
+  async getNeoznacene(faks) {
+    const user = auth.getUser();
 
-        return data
-    },
-    async getOznacene(faks){
-        const user = auth.getUser()
-        
-        let response = await Services.post(`predavaci/ispunjeni/${faks}`,{
-            email: user.email
-        })
-        let data = await response.data
+    let response = await Services.post(`predavaci/${faks}`, {
+      email: user.email,
+    });
+    let data = await response.data;
 
-        return data
-    }
-}
+    return data;
+  },
+  async getOznacene(faks) {
+    const user = auth.getUser();
+
+    let response = await Services.post(`predavaci/ispunjeni/${faks}`, {
+      email: user.email,
+    });
+    let data = await response.data;
+
+    return data;
+  },
+};
 
 const anketa = {
-    async create(kljuc, forma) {
-        let response = await Services.post(`/anketa/${kljuc}`,forma)
-        let data = await response.data
+  async create(kljuc, forma) {
+    let response = await Services.post(`/anketa/${kljuc}`, forma);
+    let data = await response.data;
 
-        return data;
-    }
-}
+    return data;
+  },
+};
 
 const auth = {
-    async login(email, password) {
-        let response = await Services.post('/user/login', {
-            email: email,
-            password: password
-        })
-        let data = await response.data
-        let status = response.status
-        
-        if(status == 400) return{
-            status: false,
-            message: data
-        }
-        else{
-            localStorage.setItem('user', JSON.stringify(data))
-            return{
-                status: true
-            }
-        };
-    },
-    async signup(email, password, faks) {
+  async login(email, password) {
+    let response = await Services.post("/user/login", {
+      email: email,
+      password: password,
+    });
+    let data = await response.data;
+    let status = response.status;
 
-        let response = await Services.post('/user/register', {
-            email: email,
-            password: password,
-            faks: faks
-        })
-        let data = await response.data
-
-        return data
-    },
-    logout() {
-        localStorage.removeItem('user')
-    },
-    getUser() {
-        return JSON.parse(localStorage.getItem("user"))
-    },
-    getToken() {
-        let user = auth.getUser()
-        if (user && user.token) {
-            return user.token
-        }
-        else return 'Korisnik nije prijavljen.'
-    },
-    getAuthenticated() {
-        let user = auth.getUser()
-        if (user && user.token) return true;
-        else return false;
-    },
-    getFaks() {
-        let user = auth.getUser()
-        if (user && user.faks) {
-            return user.faks
-        }
-        else return 'Korisnik nije prijavljen.'
-    },
-    state: {
-        get authenticated() {
-            return auth.getAuthenticated()
-        }
+    if (status == 400)
+      return {
+        status: false,
+        message: data,
+      };
+    else {
+      localStorage.setItem("user", JSON.stringify(data));
+      return {
+        status: true,
+      };
     }
-}
+  },
+  async signup(email, password, faks) {
+    let response = await Services.post("/user/register", {
+      email: email,
+      password: password,
+      faks: faks,
+    });
+    let data = await response.data;
+
+    return data;
+  },
+  logout() {
+    localStorage.removeItem("user");
+  },
+  getUser() {
+    return JSON.parse(localStorage.getItem("user"));
+  },
+  getToken() {
+    let user = auth.getUser();
+    if (user && user.token) {
+      return user.token;
+    } else return "Korisnik nije prijavljen.";
+  },
+  getAuthenticated() {
+    let user = auth.getUser();
+    if (user && user.token) return true;
+    else return false;
+  },
+  getFaks() {
+    let user = auth.getUser();
+    if (user && user.faks) {
+      return user.faks;
+    } else return "Korisnik nije prijavljen.";
+  },
+  state: {
+    get authenticated() {
+      return auth.getAuthenticated();
+    },
+  },
+};
 
 const updateProfile = {
-    async updatePassword(old_password, new_password){
-        let response = await Services.patch('/user',{
-            old_password: old_password.toString(),
-            new_password: new_password.toString()
-        })
-        let data = await response.data
-        let status = response.status
+  async updatePassword(old_password, new_password) {
+    let response = await Services.patch("/user", {
+      old_password: old_password.toString(),
+      new_password: new_password.toString(),
+    });
+    let data = await response.data;
+    let status = response.status;
 
-        if(status !== 200) return {
-            status: false,
-            message: data
-        }
-        else {
-            console.log(data);
-            return {
-                status: true,
-            }
-        }
+    if (status !== 200)
+      return {
+        status: false,
+        message: data,
+      };
+    else {
+      console.log(data);
+      return {
+        status: true,
+      };
     }
-}
-export { profs, auth, anketa, updateProfile }
+  },
+};
+export { profs, auth, anketa, updateProfile };
