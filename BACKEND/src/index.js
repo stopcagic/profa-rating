42f7f2f6fbc9a12bb294951e8efe0751e92a3d0e
MarkoSolic <58081989+MarkoSolic@users.noreply.github.com>
2020-09-04 22:07:16 +0200

    enviroment variables

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -1,24 +1,24 @@
-import express, { urlencoded } from 'express';
-import cors from 'cors'
-import dotenv from 'dotenv'
-import CookieParser from 'cookie-parser'
+import express, { urlencoded } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import CookieParser from "cookie-parser";
 
-import auth from './routes/auth'
-import predavaci from './routes/predavaci'
-import forma from './routes/forma'
+import auth from "./routes/auth";
+import predavaci from "./routes/predavaci";
+import forma from "./routes/forma";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-dotenv.config()
+dotenv.config();
 
-app.use(cors())
-app.use(urlencoded({ extended: true }))
-app.use(express.json())
-app.use(CookieParser())
+app.use(cors());
+app.use(urlencoded({ extended: true }));
+app.use(express.json());
+app.use(CookieParser());
 
-app.use('/predavaci', predavaci)
-app.use('/user', auth)
-app.use('/anketa', forma)
+app.use("/predavaci", predavaci);
+app.use("/user", auth);
+app.use("/anketa", forma);
 
-app.listen(port, () => console.log(`Slušam na portu ${port}!`))
+app.listen(port, () => console.log(`Slušam na portu ${port}!`));
